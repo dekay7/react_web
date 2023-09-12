53c95be82f26305a2fa50e365b37e04a0b9ecde0
dekay7 <darshkapadia@gmail.com>
2023-09-12 15:47:38 -0500

    github-pages setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Currently under development...
         </p>
         <a
           className="App-link"
